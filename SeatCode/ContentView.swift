412220ba988b59da1f3955fa9f59649f944b2e70
Diego Ferrari <dieferhau@me.com>
2022-05-26 12:16:01 +0200

    Initial Commit

---
+++ SeatCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeatCode
+//
+//  Created by Diego on 26/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeatCode/SeatCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatCodeApp.swift
+//  SeatCode
+//
+//  Created by Diego on 26/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SeatCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
